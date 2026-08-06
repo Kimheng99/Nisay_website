92f072f38f2bb719efa44b3e4d33036413d529e7
kimheng99 <nutkimheng000@gmail.com>
2026-08-06 16:08:15 +0700

    Initial commit

---
+++ server/server.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import express, { Request, Response } from "express";
+import cors from 'cors';
+
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+app.get('/', (req: Request, res: Response) => {
+    res.send("Server is live! ");
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`)
+})
+
+
+
