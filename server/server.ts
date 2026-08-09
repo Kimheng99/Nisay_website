ff32105740a0b3f0007766ecc6e3b2166cec2ec6
kimheng99 <nutkimheng000@gmail.com>
2026-08-10 01:40:42 +0700

    add routes and authRoutes

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from 'cors';
+import authRouter from "./routes/authRoutes.js";
 
 
 
@@ -13,6 +14,13 @@ app.get('/', (req: Request, res: Response) => {
     res.send("Server is live! ");
 });
 
+app.use('/api/auth', authRouter);
+
+// Error handling
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    res.status(500).json({message: error.message});
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`)
